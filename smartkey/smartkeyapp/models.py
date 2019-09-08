2157aae708d6e9234d74f50ea909eb0fe3a0737a
Takumi Tachibana <taku3.tfbb@icloud.com>
2019-09-08 14:13:08 +0900

    add model file

--- smartkey/smartkeyapp/models.py
+++ smartkey/smartkeyapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Distance(models.Model):
+    class Meta:
+        db_table="distance"
+        verbose_name="距離"
+
+    length = models.Floatfield(verbose_name="距離")
+    measured_time = models.DateTimeField(verbose_name="計測日時", default=timezone.now)
